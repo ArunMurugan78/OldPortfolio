40f4765c233c6a114d376706582adf4f4eb10202
ArunMurugan78 <arunmurugan200@gmail.com>
2020-05-16 18:59:31 +0530

    my portfolio ver1

--- js/index.js
+++ js/index.js
@@ -9,3 +9,25 @@ const changeText = () => {
     setTimeout(changeText, 3000);
 }
 changeText();
+const scrollto = document.querySelector('#scrollto');
+document.querySelector('#downarrow').addEventListener('click', (event) => {
+    scrollto.scrollIntoView({ behavior: "smooth" })
+})
+
+
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('#navbar');
+    console.log("hello")
+        //  console.log(window.scrollY, window.innerHeight, scrollto.scrollHeight)
+    if (window.scrollY >= scrollto.scrollHeight && window.innerHeight * 0.9 <= window.scrollY) {
+        navbar.className = navbar.className.replace('navbar-theme-2', '');
+        navbar.className = navbar.className.replace('navbar-theme-1', '') + " navbar-theme-2 ";
+        console.log(navbar.className);
+
+    } else {
+        navbar.className = navbar.className.replace('navbar-theme-1', '');
+        navbar.className = navbar.className.replace('navbar-theme-2', '') + " navbar-theme-1 ";
+        console.log(navbar.className);
+    }
+})
